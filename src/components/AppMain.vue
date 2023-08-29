3b63de8e5647c8e8324bf29b27695cca7f160fc8
marcobusacca <marcobusacca01@gmail.com>
2023-08-29 18:09:25 +0200

    [MILESTONE-4] layout projects details in AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,7 +9,7 @@ export default {
     },
     data() {
         return {
-            baseUrl: 'http://localhost:8000/api',
+            baseUrl: 'http://localhost:8000',
             loading: true,
             projects: [],
         }
@@ -22,11 +22,13 @@ export default {
 
             this.loading = true;
 
-            axios.get(`${this.baseUrl}/projects`).then((response) => {
+            axios.get(`${this.baseUrl}/api/projects`).then((response) => {
 
                 if (response.data.success) {
 
                     this.projects = response.data.results;
+
+                    console.log(response.data.results)
 
                     this.loading = false;
                 }
@@ -38,13 +40,25 @@ export default {
 
 <!-- TEMPLATE HTML -->
 <template lang="">
-    <div class="container">
-        <div class="row">
+    <div class="container pb-5">
+        <div class="row justify-content-center">
             <div class="col-12 py-5">
                 <h1 class="text-center">vite-boolfolio</h1>
             </div>
             <div class="col-12 d-flex justify-content-center align-items-center py-5" v-if="loading">
                 <AppLoader/>
+            </div>
+            <div class="col-4 my-4" v-else v-for="project in projects" :key="project.id">
+                <div class="card h-100">
+                    <img class="card-img-top img-fluid" :src="`${baseUrl}/storage/${project.cover_image}`" :alt="`${project.title}-image`">
+                    <div class="card-body h-50">
+                        <h5 class="card-title py-2" v-text="project.title"></h5>
+                        <p class="card-text py-2" v-text="project.description"></p>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a href="#" class="btn btn-primary">Visualizza Progetto</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
