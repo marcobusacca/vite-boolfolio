f0286a928452820844ec880c4f345472ad5db1dd
marcobusacca <marcobusacca01@gmail.com>
2023-08-30 17:10:13 +0200

    Esercizio Completato

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -13,6 +13,20 @@ export default {
             <div class="col-12 py-5">
                 <h1 class="text-center">Homepage</h1>
             </div>
+            <!-- HOME PAGE DESCRIPTION -->
+            <div class="col-12 py-5">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed metus non quam efficitur fringilla semper quis dolor. Etiam magna neque, condimentum sed metus ac, rhoncus ornare justo. Aenean mollis justo sit amet fermentum iaculis. Etiam fringilla, justo sed ornare tempus, lacus mi egestas turpis, et convallis ex ipsum non dolor. Phasellus sit amet faucibus odio. Nulla non condimentum nunc, hendrerit elementum risus. Mauris auctor dolor nec nisl viverra fermentum. Quisque nec nibh lorem. Nulla pharetra fringilla odio, quis vulputate neque suscipit nec.
+                    <br><br>
+                    Nullam quis facilisis est. Praesent in leo facilisis, tincidunt tellus in, posuere arcu. Nam accumsan ipsum augue, in ornare nisi malesuada ac. Sed eleifend fermentum purus sed sollicitudin. Donec ut sodales ante. Nulla auctor purus non turpis dapibus, sit amet tincidunt dui tincidunt. Sed id tellus nec eros accumsan rutrum. Fusce risus dui, fermentum eu finibus et, elementum eu libero. Sed maximus, sem at facilisis viverra, metus est sodales enim, consectetur porta sapien risus laoreet enim. Sed vel tortor magna. Ut vel consectetur lorem, vel varius velit. Aliquam fermentum ex sed dictum aliquet.
+                    <br><br>
+                    Nunc tincidunt eu turpis quis gravida. Quisque condimentum sodales nisl, ut rutrum nisl tempus sed. Suspendisse potenti. Vestibulum consectetur eros ut leo sagittis, at bibendum lorem ultrices. Ut risus libero, bibendum ut vulputate quis, congue ac nibh. Mauris eu tortor laoreet, congue augue eget, molestie risus. Mauris mauris urna, lacinia nec posuere ut, dictum id purus. Vivamus vehicula lectus sit amet nulla interdum dictum ut eget dolor. In hac habitasse platea dictumst. Nulla eget rutrum metus.
+                    <br><br>
+                    Mauris vel faucibus justo. Fusce accumsan neque et sagittis maximus. Nullam suscipit, nisi ut pulvinar lacinia, ante felis tincidunt ex, eget pulvinar metus purus eget quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In eget est in odio porttitor accumsan. Quisque eu ornare arcu. Quisque laoreet ipsum at lacus laoreet, nec facilisis orci auctor. Maecenas consequat dolor ac urna convallis gravida. Praesent lobortis commodo ante. Suspendisse gravida dui vel risus lobortis, eu viverra est sagittis. Suspendisse et scelerisque ligula, faucibus dictum erat. Nulla facilisi. Suspendisse hendrerit sagittis elementum. Quisque volutpat risus quis elementum lacinia. Morbi laoreet magna tincidunt velit finibus ornare quis sodales enim. Sed facilisis quis tortor id mollis.
+                    <br><br>
+                    Nulla feugiat sapien sit amet laoreet lobortis. Quisque commodo tincidunt nunc id viverra. Mauris in ornare orci, finibus semper nulla. Aliquam erat volutpat. Mauris maximus vehicula suscipit. Fusce placerat sapien vitae dolor ultricies, ut ornare turpis suscipit. Suspendisse sed condimentum nisl. Ut cursus blandit felis nec auctor. Integer ut elit eu urna maximus aliquam. Phasellus sed congue ex, et bibendum elit. Praesent varius, justo ut cursus aliquam, dui dolor tincidunt diam, ut auctor quam augue vel metus.
+                </p>
+            </div>
         </div>
     </div>
 </template>
