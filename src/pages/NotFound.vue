a4ad4679883bafbef5806acb82d60f140fb3392a
marcobusacca <marcobusacca01@gmail.com>
2023-08-31 18:39:02 +0200

    [FEAT] page NotFound vue

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,24 @@
+<!-- JAVASCRIPT & VUE.JS -->
+<script>
+export default {
+
+}
+</script>
+
+<!-- TEMPLATE HTML -->
+<template lang="">
+    <div class="container pb-5">
+        <div class="row justify-content-center">
+            <!-- NOT FOUND TITLE -->
+            <div class="col-12 py-5">
+                <h1 class="text-center my-5 py-5">Not Found</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<!-- STYLE SCSS -->
+<style lang="scss">
+// IMPORTO GENERALS.SCSS
+@use '../styles/generals.scss' as *;
+</style>
