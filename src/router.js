9d8e542e9d7565fb99311aa0d599a4df832d2763
marcobusacca <marcobusacca01@gmail.com>
2023-08-31 15:54:06 +0200

    [FEAT] router.js add single-project route

--- src/router.js
+++ src/router.js
@@ -4,10 +4,16 @@ import { createRouter, createWebHistory } from 'vue-router';
 // IMPORTARE TUTTE LE PAGINE DALLA CARTELLA "PAGES"
 import HomePage from './pages/HomePage.vue';
 import AboutUs from './pages/AboutUs.vue';
+
 import ProjectsList from './pages/ProjectsList.vue';
+import SingleProject from './pages/SingleProject.vue';
+
 import TypesList from './pages/TypesList.vue';
+
 import TechnologiesList from './pages/TechnologiesList.vue';
+
 import Contacts from './pages/Contacts.vue';
+
 
 // CREIAMO IL ROUTER CON LE SUE ROTTE
 const router = createRouter({
@@ -29,6 +35,11 @@ const router = createRouter({
             path: '/projects',
             name: 'projects-list',
             component: ProjectsList,
+        },
+        {
+            path: '/projects/:slug',
+            name: 'single-project',
+            component: SingleProject,
         },
         {
             path: '/types',
