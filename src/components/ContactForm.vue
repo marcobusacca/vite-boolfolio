d963de5a1894828bc9bdfc2df1970ec5addc16b8
marcobusacca <marcobusacca01@gmail.com>
2023-09-01 13:32:54 +0200

    [FEAT] ContactForm sendForm method

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -20,7 +20,39 @@ export default {
         }
     },
     methods: {
+        sendForm() {
 
+            // SALVO I DATI DI INPUT DELL'UTENTE
+            const form_data = {
+
+                name: this.name,
+                email: this.email,
+                content: this.content,
+
+            };
+
+            // SVUOTO L'OGGETTO CONTENTE I MESSAGGI DI ERRORE
+            this.errors = {};
+
+            // EFFETTUIAMO LA CHIAMATA AXIOS IN POST
+            axios.post(`${this.store.baseUrl}/api/contacts`, form_data).then((response) => {
+
+                if (response.data.success) {
+
+                    // RIPULISCO I DATI DI INPUT
+                    this.name = '';
+                    this.email = '';
+                    this.content = '';
+
+                    this.$router.push({ name: 'thank-you' });
+
+                } else {
+
+                    // SALVO I MESSAGGI DI ERRORE NELL'OGGETTO ERRORS
+                    this.errors = response.data.errors;
+                }
+            });
+        }
     },
 }
 </script>
