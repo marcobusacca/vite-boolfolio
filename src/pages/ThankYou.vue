22d3cb2c5b8fb2738af6b318846308365112ecb3
marcobusacca <marcobusacca01@gmail.com>
2023-09-01 14:17:25 +0200

    Esercizio Completato

--- src/pages/ThankYou.vue
+++ src/pages/ThankYou.vue
@@ -9,13 +9,13 @@ export default {
 <template lang="">
     <div class="container pb-5">
         <div class="row justify-content-center">
-                <!-- THANK YOU TITLE -->
+            <!-- THANK YOU TITLE -->
             <div class="col-12 py-5">
                 <h1 class="text-center">Grazie per averci contattato</h1>
-                <p>
+                <p class="text-center my-4">
                     Un nostro incaricato ti ricontatterà nel più breve tempo possibile!
                     <br>
-                    Intanto puoi proseguire nella navigazione del nostro sito!
+                    Intanto puoi proseguire navigando nel nostro sito!
                 </p>
             </div>
         </div>
