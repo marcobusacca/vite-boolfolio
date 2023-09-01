345784ca598726b2901c8ca0002fa9cc4570cc1c
marcobusacca <marcobusacca01@gmail.com>
2023-09-01 13:31:55 +0200

    [FEAT] component ContactForm layout

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,30 @@
+<!-- JAVASCRIPT & VUE.JS -->
+<script>
+import { store } from '../store';
+
+import axios from 'axios';
+
+export default {
+    components: {
+
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
+
+<!-- TEMPLATE HTML -->
+<template lang="">
+</template>
+
+<!-- STYLE SCSS -->
+<style lang="scss">
+// IMPORTO GENERALS.SCSS
+@use '../styles/generals.scss' as *;
+</style>
