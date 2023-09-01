348731b9ff631dacacbf8ab0e704a7eed8b1f8a8
marcobusacca <marcobusacca01@gmail.com>
2023-09-01 14:14:06 +0200

    [FEAT] page ThankYou vue

---
+++ src/pages/ThankYou.vue
@@ -0,0 +1,29 @@
+<!-- JAVASCRIPT & VUE.JS -->
+<script>
+export default {
+
+}
+</script>
+
+<!-- TEMPLATE HTML -->
+<template lang="">
+    <div class="container pb-5">
+        <div class="row justify-content-center">
+                <!-- THANK YOU TITLE -->
+            <div class="col-12 py-5">
+                <h1 class="text-center">Grazie per averci contattato</h1>
+                <p>
+                    Un nostro incaricato ti ricontatterà nel più breve tempo possibile!
+                    <br>
+                    Intanto puoi proseguire nella navigazione del nostro sito!
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<!-- STYLE SCSS -->
+<style lang="scss">
+// IMPORTO GENERALS.SCSS
+@use '../styles/generals.scss' as *;
+</style>
